6b664a9c539ed835a7dc64f4b31f21dd861fce5b
Samuli Penttila <samuli@vaadin.com>
2019-08-23 09:21:48 +0300

    Fixed bug with adding too many cells with same id. Update V13. Compare with Grid Add API for total size and cell size

--- src/main/java/org/vaadin/samuli/InfiniteGrid.java
+++ src/main/java/org/vaadin/samuli/InfiniteGrid.java
@@ -3,17 +3,17 @@ package org.vaadin.samuli;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vaadin.flow.component.ClientCallable;
-import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.HasSize;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.polymertemplate.Id;
 import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
+import com.vaadin.flow.data.renderer.Renderer;
+import com.vaadin.flow.data.renderer.TextRenderer;
+import com.vaadin.flow.dom.Element;
 import com.vaadin.flow.templatemodel.TemplateModel;
-
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.List;
@@ -21,109 +21,132 @@ import java.util.stream.Collectors;
 
 @Tag("infinite-grid")
 @HtmlImport("src/infinite-grid.html")
-public class InfiniteGrid extends PolymerTemplate<InfiniteGrid.ExampleModel> {
-    @Id("storage")
-    private Div storage;
+public class InfiniteGrid extends PolymerTemplate<InfiniteGrid.ExampleModel> implements HasSize {
+  @Id("storage")
+  private Div storage;
+  private int maxSize = 0;
 
-    /**
-     * Template model which defines the single "value" property.
-     */
-    public interface ExampleModel extends TemplateModel {
+  private Renderer<Pair> renderer = new TextRenderer<>(pair -> String.format("(%d, %d)", pair.getX(), pair.getY()));
 
-        void setValue(String name);
+  public InfiniteGrid() {
+    // Set the initial value to the "value" property.
+
+  }
+
+  public void setValue(String value) {
+    getModel().setValue(value);
+  }
+
+  @ClientCallable
+  public void getContent(String[] stuff) {
+    maxSize = Math.max(stuff.length * 4, maxSize);
+    if (storage.getElement().getChildCount() > maxSize) {
+      for (int i = 0; i < maxSize; i++) {
+        storage.getElement().removeChild(0);
+      }
     }
 
-    public InfiniteGrid() {
-        // Set the initial value to the "value" property.
+    List<Pair> retvalue = Arrays.stream(stuff).map(str -> {
+      String[] pair = str.split("_");
+      Pair p = new Pair(Integer.valueOf(pair[0]), Integer.valueOf(pair[1]));
+      String msg = String.format("<b>(%d, %d) [[cellWidth]]</b>", p.getX(), p.getY());
+      p.setM(msg);
+      //            Component c = ((p.getX() + p.getY()) % 2) == 0 ? new Button(msg, l -> System.out.println("got " + msg)) : new Label(msg);
+//      Element c = new Element("button");
+//      c.setAttribute("id", "id" + p.getX() + "_" + p.getY());
+//      c.setText(msg);
+//      storage.getElement().appendChild(c);
+      return p;
+    }).collect(Collectors.toList());
 
+    try {
+      getElement().callFunction("setContent", new ObjectMapper().writeValueAsString(retvalue));
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void setCellSize(int width, int height) {
+    getModel().setCellWidth(width);
+    getModel().setCellHeight(height);
+  }
+
+  public void setItemCount(int x, int y) {
+    getModel().setItemX(x);
+    getModel().setItemY(y);
+  }
+
+  @ClientCallable
+  public void handleClick() {
+    System.out.println("clicked");
+    final UI ui = UI.getCurrent();
+    new Thread(() -> {
+      try {
+        Thread.sleep(10000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      ui.access(() -> whenServerResponds());
+
+    }).start();
+  }
+
+  public void whenServerResponds() {
+    getElement().callFunction("whenServerResponds");
+  }
+
+  /**
+   * Template model which defines the single "value" property.
+   */
+  public interface ExampleModel extends TemplateModel {
+
+    void setValue(String name);
+
+    void setCellWidth(Integer width);
+
+    void setCellHeight(Integer height);
+
+    void setItemX(Integer x);
+
+    void setItemY(Integer y);
+  }
+
+
+  public static class Pair implements Serializable {
+    private Integer x;
+    private Integer y;
+    private String m;
+
+    public Pair() {
     }
 
-    public void setValue(String value) {
-        getModel().setValue(value);
+    public Pair(Integer x, Integer y) {
+      this.x = x;
+      this.y = y;
     }
 
-    public static class Pair implements Serializable {
-        private Integer x;
-        private Integer y;
-        private String m;
-
-        public Pair() {
-        }
-
-        public Pair(Integer x, Integer y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public Integer getX() {
-            return x;
-        }
-
-        public void setX(Integer x) {
-            this.x = x;
-        }
-
-        public Integer getY() {
-            return y;
-        }
-
-        public void setY(Integer y) {
-            this.y = y;
-        }
-
-        public String getM() {
-            return m;
-        }
-
-        public void setM(String m) {
-            this.m = m;
-        }
+    public Integer getX() {
+      return x;
     }
 
-    private int maxSize = 0;
-
-    @ClientCallable
-    public void getContent(String[] stuff) {
-        maxSize = Math.max(stuff.length, maxSize);
-        if (storage.getElement().getChildCount() > maxSize) {
-            for(int i=0; i < maxSize;i++) {
-//                storage.getElement().removeChild(0);
-            }
-        }
-//        storage.removeAll();
-        List<Pair> retvalue = Arrays.stream(stuff).map(str -> {
-            String[] pair = str.split("_");
-            Pair p = new Pair(Integer.valueOf(pair[0]), Integer.valueOf(pair[1]));
-            p.setM(String.format("(%d, %d)", p.getX(), p.getY()));
-            Component c = ((p.getX()+p.getY())%2) == 0 ?new Button(p.getM(), l-> System.out.println("got "+p.getM())):new Label(p.getM());
-            c.setId("id"+p.getX()+"_"+p.getY());
-            storage.add(c);
-            return p;
-        }).collect(Collectors.toList());
-
-        try {
-            getElement().callFunction("setContent", new ObjectMapper().writeValueAsString(retvalue));
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
+    public void setX(Integer x) {
+      this.x = x;
     }
 
-    @ClientCallable
-    public void handleClick() {
-        System.out.println("clicked");
-        final UI ui = UI.getCurrent();
-        new Thread(() -> {
-            try {
-                Thread.sleep(10000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            ui.access(() -> whenServerResponds());
-
-        }).start();
+    public Integer getY() {
+      return y;
     }
 
-    public void whenServerResponds() {
-        getElement().callFunction("whenServerResponds");
+    public void setY(Integer y) {
+      this.y = y;
     }
+
+    public String getM() {
+      return m;
+    }
+
+    public void setM(String m) {
+      this.m = m;
+    }
+  }
 }
--- src/test/java/org/vaadin/samuli/DemoView.java
+++ src/test/java/org/vaadin/samuli/DemoView.java
@@ -1,13 +1,29 @@
 package org.vaadin.samuli;
 
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.data.provider.ListDataProvider;
+import com.vaadin.flow.data.renderer.TemplateRenderer;
 import com.vaadin.flow.router.Route;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Route("")
 public class DemoView extends Div {
 
-    public DemoView() {
-        add(new InfiniteGrid());
-        setSizeFull();
-    }
+  public DemoView() {
+    InfiniteGrid infiniteGrid = new InfiniteGrid();
+    infiniteGrid.setWidth("100%");
+    infiniteGrid.setHeight("50%");
+    infiniteGrid.setCellSize(200, 40);
+    infiniteGrid.setItemCount(100000, 100000);
+    add(infiniteGrid);
+    Grid<Object> grid = new Grid<>();
+    grid.setDataProvider(new ListDataProvider<Object>(IntStream.range(0, 100000).mapToObj(i -> i).collect(Collectors.toList())));
+    grid.setWidth("100%");
+    grid.setHeight("50%");
+    for (int loop=0; loop < 9; loop++) grid.addColumn(TemplateRenderer.of(""+(loop+1)));
+    add(grid);
+    setSizeFull();
+  }
 }
